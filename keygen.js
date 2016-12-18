4ccdb0e556e2ee6a96aab1adb6ec801f78ce11a8
Will White <w.white9@icloud.com>
2016-12-18 20:19:23 +0000

    uuid (random string) for seed

--- keygen.js
+++ keygen.js
@@ -1,4 +1,10 @@
 var EthUtil = require("ethereumjs-util")
+var Web3 = require("web3")
+var uuidV4 = require('uuid/v4');
+
+var randomString = uuidV4()
+
+var web3 = new Web3
 
 var hexToBytes = function (hex) {
   for (var bytes = [], c = 0; c < hex.length; c+=2)
@@ -10,4 +16,11 @@ var privateKeyToAddress = function (privateKey) {
   return `0x${EthUtil.privateToAddress(hexToBytes(privateKey)).toString("hex")}`
 }
 
-console.log(privateKeyToAddress(process.argv[2]));
+var stringToPrivateKey = function (seed) {
+  return web3.sha3(seed)
+  .slice(2) // takes off 0x
+}
+
+var privateKey = stringToPrivateKey(randomString)
+
+console.log(`private key: ${privateKey}\naddress: ${privateKeyToAddress(privateKey)}`);
